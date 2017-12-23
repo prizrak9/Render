c14e12cc95206230e596fb082ea216ee211157fb
prizrak9 <pavelgolotuk@gmail.com>
2017-12-23 18:16:09 +0200

    quaternion
    fixed FromVector

--- Quaternion.cs
+++ Quaternion.cs
@@ -23,8 +23,18 @@ namespace render
         {
             double angleBetweenVectorAndUp = Vector.AngleBetween(ob, Vector.Up);
             double s = Math.Sin(angleBetweenVectorAndUp / 2);
+            Vector third = ob * Vector.Up;
 
-            return new Quaternion(Math.Cos(angleBetweenVectorAndUp / 2), ob.X * s, ob.Y * s, ob.Z * s);
+            return new Quaternion(Math.Cos(angleBetweenVectorAndUp / 2), third.X * s, third.Y * s, third.Z * s);
+        }
+
+        public static Quaternion FromVector(Vector ob1, Vector ob2)
+        {
+            double angleBetween = Vector.AngleBetween(ob1, ob2);
+            double s = Math.Sin(angleBetween / 2);
+            Vector third = ob1 * ob2;
+
+            return new Quaternion(Math.Cos(angleBetween / 2), third.X * s, third.Y * s, third.Z * s);
         }
     }
 }
