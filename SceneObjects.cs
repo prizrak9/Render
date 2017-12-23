d3eae62c929b74eb1f28108c6a481f8432861eab
prizrak9 <pavelgolotuk@gmail.com>
2017-12-23 16:46:25 +0200

    vector
    added magnitude, square magnitude, fromEuler, AngleBetween

--- SceneObjects.cs
+++ SceneObjects.cs
@@ -9,27 +9,41 @@ namespace render
 {
     struct Vector
     {
-        public double x, y, z;
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
 
-        public static Vector operator/(Vector ob1, double ob2)
-        {
-            double x = ob1.x / ob2, y = ob1.y / ob2, z = ob1.z / ob2;
-            return new Vector { x = x, y = y, z = z };
-        }
+        public double Magnitude => Math.Sqrt(SquareMagnitude);
+        public double SquareMagnitude => X * X + Y * Y + Z * Z;
+
 
         public Vector(double x, double y, double z)
         {
-            this.x = x;
-            this.y = y;
-            this.z = z;
+            X = x;
+            Y = y;
+            Z = z;
         }
 
         public static Vector Up => new Vector(0, 0, 1);
         public static Vector Left => new Vector(0, 1, 0);
         public static Vector Forward => new Vector(1, 0, 0);
-    }
 
-    struct Polyline
+
+        public static Vector operator /(Vector ob1, double ob2) =>
+             new Vector(ob1.X / ob2, ob1.Y / ob2, ob1.Z / ob2);
+
+        public static Vector FromEuler(double angX, double angY, double angZ) =>
+            new Vector(Math.Cos(angX), Math.Cos(angY), Math.Cos(angZ));
+
+        public static double ScalarMultiply(Vector a, Vector b) =>
+            a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+
+        public static double AngleBetween(Vector a, Vector b) =>
+            Math.Acos(ScalarMultiply(a, b) / a.Magnitude / b.Magnitude);
+
+}
+
+struct Polyline
     {
         public double[] points;
     }
