c3f3030b90eab997bc46c35a84fcab307c9185a9
prizrak9 <pavelgolotuk@gmail.com>
2017-12-24 17:38:19 +0200

    beautification
    Moved Vector3, Vector2 to separate files.
    Rewrote methods definition style from lambda symbol to normal definition style with brackets and 'return'
    Put some regions to separate logic in Vector3

--- Vector3.cs
+++ Vector3.cs
@@ -2,28 +2,12 @@
 
 namespace render
 {
-    struct Vector2
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-
-        public Vector2(double x, double y)
-        {
-            X = x;
-            Y = y;
-        }
-        public static Vector2 operator /(Vector2 ob1, double ob2) =>
-            new Vector2(ob1.X / ob2, ob1.Y / ob2);
-
-        public static Vector2 operator+(Vector2 ob1, double ob2)
-        {
-            return new Vector2(ob1.X + ob2, ob1.Y + ob2);
-        }
-
-    }
-
     struct Vector3
     {
+        public static Vector3 Up => new Vector3(0, 0, 1);
+        public static Vector3 Left => new Vector3(0, 1, 0);
+        public static Vector3 Forward => new Vector3(1, 0, 0);
+
         public double X { get; set; }
         public double Y { get; set; }
         public double Z { get; set; }
@@ -39,54 +23,61 @@ namespace render
             Z = z;
         }
 
-        public Vector3 SubstructFrom(Vector3 ob) =>
-            ob - this;
-
-        public Vector3 Normalize()
+        public Vector3 Normalized()
         {
             double magn = Magnitude;
             return new Vector3(X / magn, Y / magn, Z / magn);
         }
 
-        public static Vector3 Up => new Vector3(0, 0, 1);
-        public static Vector3 Left => new Vector3(0, 1, 0);
-        public static Vector3 Forward => new Vector3(1, 0, 0);
+        #region Operator
 
-
-        public static Vector3 operator /(Vector3 ob1, double ob2) =>
-             new Vector3(ob1.X / ob2, ob1.Y / ob2, ob1.Z / ob2);
-
+        public static Vector3 operator /(Vector3 ob1, double ob2)
+        {
+            return new Vector3(ob1.X / ob2, ob1.Y / ob2, ob1.Z / ob2);
+        }
 
         public static Vector3 operator *(Vector3 ob1, Vector3 ob2)
         {
             return new Vector3(ob1.X * ob2.Z - ob1.Z * ob2.Y, ob1.Z * ob2.X - ob1.X * ob2.Z, ob1.X * ob2.Y - ob1.Y * ob2.X);
         }
+
         public static Vector3 operator *(Vector3 ob1, double ob2)
         {
             return new Vector3(ob1.X * ob2, ob1.Y * ob2, ob1.Z * ob2);
         }
+
         public static Vector3 operator *(double ob1, Vector3 ob2)
         {
             return new Vector3(ob1 * ob2.X, ob1 * ob2.Y, ob1 * ob2.Z);
         }
 
-        public static Vector3 operator -(Vector3 ob1, Vector3 ob2) =>
-            new Vector3(ob1.X - ob2.X, ob1.Y - ob2.Y, ob1.Z - ob2.Z);
+        public static Vector3 operator -(Vector3 ob1, Vector3 ob2)
+        {
+            return new Vector3(ob1.X - ob2.X, ob1.Y - ob2.Y, ob1.Z - ob2.Z);
+        }
 
+        #endregion
 
-        public static Vector3 FromEuler(double angX, double angY, double angZ) =>
-            new Vector3(Math.Cos(angX), Math.Cos(angY), Math.Cos(angZ));
+        #region Static
 
-        public static double ScalarMultiply(Vector3 a, Vector3 b) =>
-            a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        public static Vector3 FromEuler(double angX, double angY, double angZ)
+        {
+            return new Vector3(Math.Cos(angX), Math.Cos(angY), Math.Cos(angZ));
+        }
 
-        public static Vector3 VectorMultiply(Vector3 a, Vector3 b) =>
-            a * b;
+        public static double ScalarMultiply(Vector3 a, Vector3 b)
+        {
+            return a.X * b.X + a.Y * b.Y + a.Z * b.Z;
+        }
 
+        public static Vector3 VectorMultiply(Vector3 a, Vector3 b)
+        {
+            return a * b;
+        }
 
         public static double AngleBetween(Vector3 a, Vector3 b)
         {
-            return Math.Acos(ScalarMultiply(a, b) / a.Magnitude / b.Magnitude);
+            return Math.Acos(CosBetween(a, b));
         }
 
         public static double CosBetween(Vector3 a, Vector3 b)
@@ -136,9 +127,10 @@ namespace render
         /// <returns></returns>
         //public static Vector3 ProjectVectorToVector(Vector3 ob1, Vector3 ob2)
         //{
-        //    return ScalarMultiply(ob1, ob2) / ob2.Magnitude * ob2.Normalize();
+        //    return ScalarMultiply(ob1, ob2) / ob2.Magnitude * ob2.Normalized();
 
         //}
 
+        #endregion
     }
 }
