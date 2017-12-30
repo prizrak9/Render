8a1174129c90f067d9cf2716acf641d92e87b6ba
Alex314 <san-y-a@mail.ru>
2017-12-31 01:28:45 +0200

    Rotation with Quaternion

--- Quaternion.cs
+++ Quaternion.cs
@@ -4,22 +4,63 @@ namespace render
 {
     struct Quaternion
     {
-        public double W { get; set; }
-        public double X { get; set; }
-        public double Y { get; set; }
-        public double Z { get; set; }
+        public double s { get; set; }
+        public Vector3 v { get; set; }
 
-        
+        public double SquareMagnitude => s * s + v.SquareMagnitude;
+        public double Magnitude => Math.Sqrt(SquareMagnitude);
+        public Quaternion Conjugate => new Quaternion(s, -1 * v);
+        public Quaternion Inverse => Conjugate / SquareMagnitude;
+
         public Quaternion(double w, double x, double y, double z)
         {
-            W = w;
-            X = x;
-            Y = y;
-            Z = z;
+            s = w;
+            v = new Vector3(x, y, z);
         }
 
+        public Quaternion(double ss, Vector3 vv)
+        {
+            s = ss;
+            v = new Vector3(vv.X, vv.Y, vv.Z);
+        }
 
-        public static Quaternion FromVector(Vector3 ob)
+        public Quaternion(Vector3 vv)
+        {
+            s = 0;
+            v = new Vector3(vv.X, vv.Y, vv.Z);
+        }
+
+        #region Operator
+
+        public static Quaternion operator +(Quaternion ob1, Quaternion ob2)
+        {
+            return new Quaternion(ob1.s + ob2.s, ob1.v + ob2.v);
+        }
+
+        public static Quaternion operator *(Quaternion ob1, Quaternion ob2)
+        {
+            return new Quaternion(ob1.s * ob2.s - Vector3.ScalarMultiply(ob1.v, ob2.v), ob1.s * ob2.v + ob2.s * ob1.v + ob1.v * ob2.v);
+        }
+
+        public static Quaternion operator /(Quaternion ob, double a)
+        {
+            return new Quaternion(ob.s / a, ob.v / a);
+        }
+
+        #endregion
+
+        #region Static
+
+        public static Vector3 Rotate(Vector3 v, Quaternion q)
+        {
+            return (q * new Quaternion(v) * q.Inverse).v;
+        }
+
+        #endregion
+
+
+        /*
+         public static Quaternion FromVector(Vector3 ob)
         {
             double angleBetweenVectorAndUp = Vector3.AngleBetween(ob, Vector3.Up);
             double s = Math.Sin(angleBetweenVectorAndUp / 2);
@@ -36,5 +77,6 @@ namespace render
 
             return new Quaternion(Math.Cos(angleBetween / 2), third.X * s, third.Y * s, third.Z * s);
         }
+        */
     }
 }
