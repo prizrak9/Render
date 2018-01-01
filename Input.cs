085a6b936b52b7607f6c0b22271bc26f141b49be
prizrak9 <pavelgolotuk@gmail.com>
2018-01-01 14:49:02 +0200

    Vertical + beautification

--- Input.cs
+++ Input.cs
@@ -14,41 +14,51 @@ namespace render
                 return right - left;
             }
         }
+        private static class VerticalAxe
+        {
+            public static double up = 0, down = 0;
+
+            public static double GetValue()
+            {
+                return up - down;
+            }
+        }
 
         public static class Update
         {
+            static Func<Key, bool> isDown = Keyboard.IsKeyDown;
+            static Func<Key, bool> isUp = Keyboard.IsKeyUp;
+
             public static void KeyDown(object sender, KeyEventArgs e)
             {
-                if (Keyboard.IsKeyDown(Key.A) || Keyboard.IsKeyDown(Key.Left))
-                {
-                    HorizontalAxe.left = 1;
-                }
-                if (Keyboard.IsKeyDown(Key.D) || Keyboard.IsKeyDown(Key.Right))
-                {
-                    HorizontalAxe.right = 1;
-                }
+                if (isDown(Key.A) || isDown(Key.Left))HorizontalAxe.left = 1;
+                if (isDown(Key.D) || isDown(Key.Right))HorizontalAxe.right = 1;
+
+                if (isDown(Key.W) || isDown(Key.Up))VerticalAxe.up = 1;
+                if (isDown(Key.S) || isDown(Key.Down))VerticalAxe.down = 1;
             }
 
             public static void KeyUp(object sender, KeyEventArgs e)
             {
-                if (Keyboard.IsKeyUp(Key.A) || Keyboard.IsKeyUp(Key.Left))
-                {
-                    HorizontalAxe.left = 0;
-                }
-                if (Keyboard.IsKeyUp(Key.D) || Keyboard.IsKeyUp(Key.Right))
-                {
-                    HorizontalAxe.right = 0;
-                }
+                if (isUp(Key.A) && isUp(Key.Left))HorizontalAxe.left = 0;
+                if (isUp(Key.D) && isUp(Key.Right))HorizontalAxe.right = 0;
+
+                if (isUp(Key.W) && isUp(Key.Up))VerticalAxe.up = 0;
+                if (isUp(Key.S) && isUp(Key.Down))VerticalAxe.down = 0;
             }
         }
 
-        public enum Axe { Horizontal }
+        public enum Axe { Horizontal, Vertical }
 
         public static double GetAxe(Axe axe)
         {
             switch (axe)
             {
                 case Axe.Horizontal:
+                    {
+                        return HorizontalAxe.GetValue();
+                    }
+                case Axe.Vertical:
                     {
                         return HorizontalAxe.GetValue();
                     }
